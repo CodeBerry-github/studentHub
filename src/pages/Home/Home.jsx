5c36cf0d97a1f0152c3bc884c8aee44b6e0d976b
anishNagula <anish.nagula@gmail.com>
2024-10-15 19:46:07 +0530

    added images to posts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import up from '../../assets/Icons/circle-arrow-up-01-stroke-rounded.svg';
 import add from '../../assets/Icons/add-square-stroke-rounded.svg';
 import filter from '../../assets/Icons/filter-stroke-rounded.svg';
 import search from '../../assets/Icons/search-01-stroke-rounded.svg';
+import hackathon from '../../assets/hackathon.jpg';
 
 const Home = () => {
   const [showDropdown, setShowDropdown] = useState(false);
@@ -17,7 +18,7 @@ const Home = () => {
   // Temporary data structure to store posts data. INTEGRATE WITH BACKEND 
   const [posts, setPosts] = useState([
     { id: 1, title: 'Internship at Google', subject: 'Google is offering an internship for software engineers. Apply now!', description: "Share your experiences and learnings from your internship, highlighting key projects or tasks you worked on, such as developing a new feature for their flagship product, Quantum Leap.", type: 'Internships' },
-    { id: 2, title: 'Hackathon Event', subject: 'Join the CodeFest hackathon happening next month.', description: "Talk about your team's project, the challenges you faced, and the solutions you came up with. Share photos or videos of the hackathon experience, such as the late-night coding sessions or the final pitch presentations.", type: 'Hackathons' },
+    { id: 2, title: 'Hackathon Event', subject: 'Join the CodeFest hackathon happening next month.', description: "Talk about your team's project, the challenges you faced, and the solutions you came up with. Share photos or videos of the hackathon experience, such as the late-night coding sessions or the final pitch presentations.", type: 'Hackathons', image: hackathon },
     { id: 3, title: 'Collaborate on Project', subject: 'Looking for students to collaborate on a web development project.', description: "Introduce your collaborators and explain the project you're working on, which is a renewable energy initiative aimed at developing a new type of solar panel. Share the goals, challenges, and expected outcomes.", type: 'Project Collabs' },
     { id: 4, title: 'Internship at Microsoft', subject: 'Internship, NovaTech Labs, Data Analyst', description: 'Give a glimpse into the company culture, work environment, and team dynamics. Share photos or videos of your team outings or company events, like the recent Innovation Hackathon.', type: 'Internships' },
     { id: 5, title: 'Hackathon for Beginners', subject: "Hackathon, Hacktopia, AI-Powered Language Translator", description: "Highlight the fast-paced nature of hackathons and the thrill of building something from scratch in a limited timeframe. Share a photo of your team's final prototype or a video of it in action.", type: 'Hackathons' },
@@ -86,17 +87,20 @@ const Home = () => {
             </ul>
           </div>
           <div className='home-post-container'>
-            {posts.filter(post => currentFilter === 'All' || post.type === currentFilter).map(post => (
-              <div className="home-post" key={post.id}>
-                <h3>{post.title}</h3>
-                <p className='home-subject'>{post.subject}</p>
-                <div className='home-description' style={{ display: 'none' }}>{post.description}</div>
-                <div className='home-expand'>
-                  <button className="home-post_btn">Apply Now</button>
-                  <img src={down} alt='down button' onClick={handleExpandClick} />
-                </div>
+          {posts.filter(post => currentFilter === 'All' || post.type === currentFilter).map(post => (
+            <div className="home-post" key={post.id}>
+              <h3>{post.title}</h3>
+              {post.image && (
+                <img className='home-post-img' src={post.image} alt="" />
+              )}
+              <p className='home-subject'>{post.subject}</p>
+              <div className='home-description' style={{ display: 'none' }}>{post.description}</div>
+              <div className='home-expand'>
+                <button className="home-post_btn">Apply Now</button>
+                <img src={down} alt='down button' onClick={handleExpandClick} />
               </div>
-            ))}
+            </div>
+          ))}
           </div>
         </main>
 
